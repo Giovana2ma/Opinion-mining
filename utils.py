4e9c5983e419bf99fab5cf1075ec0e03947c26ac
Giovana2ma <giovana2ma@gmail.com>
2025-02-13 10:40:00 -0300

    More analisys

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ from wordcloud import WordCloud
 import nltk
 import isodate
 from nltk.corpus import stopwords
+import unicodedata
 warnings.filterwarnings('ignore')
 
 
@@ -43,10 +44,21 @@ def filter_videos(data, terms):
     pattern = r'\b(' + '|'.join(map(re.escape, terms)) + r')\b'
 
     # Filter rows where 'Title' or 'Description' contains any of the terms
-    filtered_data = data[data['Title'].str.contains(pattern, case=False, na=False) |
-                            data['Description'].str.contains(pattern, case=False, na=False)]
+    filtered_data = data[data['Title'].str.contains(pattern, case=False, na=False)]
 
-    return filtered_data  
+    return filtered_data
+
+def remove_tilde(text):
+    if isinstance(text, str):  # Ensure it's a string
+        return unicodedata.normalize("NFD", text).encode("ascii", "ignore").decode("utf-8")
+    return text  # Return as is if not a string
+
+# Function to remove URLs, numbers, and newlines
+def remove_urls_numbers_newlines(text):
+    text_without_urls = re.sub(r'https?://\S+|www\.\S+', '', text)  # Remove URLs
+    text_without_numbers = re.sub(r'\d+', '', text_without_urls)  # Remove numbers
+    text_without_newlines = re.sub(r'\n', ' ', text_without_numbers)  # Replace newlines with spaces
+    return text_without_newlines
 
 def clean_text(text):
     """Removes stopwords, punctuation, and special characters from text."""
@@ -55,13 +67,15 @@ def clean_text(text):
     # Add custom stopwords
     custom_stopwords = {"https", "follow","instagram","veja","abril","assine" \
                         ,"bitly","bit ly","abr","2vzw8dn","confira","últimas"\
-                          ,"vejanoinsta", "br", "siga" }
+                          ,"vejanoinsta", "br", "siga"}
     stop_words.update(custom_stopwords)
 
     if pd.isna(text):  # Handle NaN values
         return ""
     text = text.lower()  # Convert to lowercase
     text = re.sub(r'\W+', ' ', text)  # Remove special characters and punctuation
+    text = remove_tilde(text)
+    text = remove_urls_numbers_newlines(text)
     words = text.split()  # Tokenize text
     words = [word for word in words if word not in stop_words]  # Remove stopwords
     return " ".join(words)
@@ -69,7 +83,7 @@ def clean_text(text):
 def generate_wordcloud(data):
     """Generates a word cloud from the 'Title' and 'Description' columns."""
     # Merge 'Title' and 'Description' columns
-    combined_text = " ".join(data["Title"].astype(str) + " " + data["Description"].astype(str))
+    combined_text = " ".join(data["Title"].astype(str))
 
     # Clean text
     cleaned_text = clean_text(combined_text)
@@ -105,24 +119,46 @@ def generate_duration_histogram(data,name):
 
         # Show plot
         plt.show()
+def generate_histogram(data,col,name):
+        # Create the figure
+        plt.figure(figsize=(8, 5))
 
-def generate_years_barplot(data,name):
-    data["PublicationDate"] = pd.to_datetime(data["PublicationDate"].str.replace("Z", "", regex=False))
-    data["upload_year"] = data["PublicationDate"].dt.year
+        # Plot histogram with KDE
+        sns.histplot(data[col], bins=10, kde=True, color="#4C72B0", edgecolor="black", alpha=0.8)
 
-    # Set Seaborn style
+        # Labels and title
+        plt.xlabel(col, fontsize=12)
+        plt.ylabel("Frequency", fontsize=12)
+        plt.title(f"Distribution of Video {col} on {name}", fontsize=14, fontweight="bold")
+
+        # Grid for readability
+        plt.grid(axis="y", linestyle="--", alpha=0.7)
+        plt.grid(axis="x", linestyle="--", alpha=0.0)
+
+        # Show plot
+        plt.show()
+
+def generate_month_year_barplot(data, name):
+    data["PublicationDate"] = pd.to_datetime(data["PublicationDate"])
+
+    data = data.dropna(subset=["PublicationDate"])
+    data["upload_month_year"] = data["PublicationDate"].dt.to_period("M")
+    count_data = data["upload_month_year"].value_counts().sort_index()
+
+    # Configurar estilo do gráfico
     sns.set_style("whitegrid")
+    plt.figure(figsize=(12, 5))
 
-    # Create figure
-    plt.figure(figsize=(8, 5))
+    # Criar o gráfico de barras
+    sns.barplot(x=count_data.index.astype(str), y=count_data.values, palette="viridis")
 
-    # Plot bar chart
-    sns.countplot(x=data["upload_year"], palette="viridis")
-
-    # Labels and title
-    plt.xlabel("Year", fontsize=12)
+    # Configurar rótulos e título
+    plt.xlabel("Month-Year", fontsize=12)
     plt.ylabel("Number of Videos", fontsize=12)
-    plt.title(f"Distribution of Videos Over the Years on {name}", fontsize=14, fontweight="bold")
+    plt.title(f"Distribution of Videos Over Time on {name}", fontsize=14, fontweight="bold")
 
-    # Show plot
+    # Rotacionar os rótulos do eixo x para melhor legibilidade
+    plt.xticks(rotation=45, ha="right")
+
+    # Exibir o gráfico
     plt.show()
